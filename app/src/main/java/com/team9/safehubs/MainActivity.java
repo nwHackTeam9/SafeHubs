408fccb97c18f964a052890f261e8c1980995766
Michael Dressner <mdressne@terpmail.umd.edu>
2021-01-09 17:29:31 -0500

    Experimentation with MapView

--- app/src/main/java/com/team9/safehubs/MainActivity.java
+++ app/src/main/java/com/team9/safehubs/MainActivity.java
@@ -4,6 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
