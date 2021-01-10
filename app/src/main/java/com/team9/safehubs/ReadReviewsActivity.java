9abfe7b74fc615f39e81b2e87005ecd73d0b7a7b
Michael Dressner <mdressne@terpmail.umd.edu>
2021-01-10 12:15:06 -0500

    Copy pasted version of ReadReviewsActivity from harsh branch

--- app/src/main/java/com/team9/safehubs/ReadReviewsActivity.java
+++ app/src/main/java/com/team9/safehubs/ReadReviewsActivity.java
@@ -1,14 +1,37 @@
 package com.team9.safehubs;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
 public class ReadReviewsActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerAdapter adapter;
+    List<ReviewBlock> reviewBlockList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_reviews);
+
+        reviewBlockList.add(new ReviewBlock("place1", (float) 3.5, "2021/01/05 12:12", "addit.comment1"));
+        reviewBlockList.add(new ReviewBlock("place2", (float) 3.0, "2021/01/02 11:16", "addit.comment2"));
+        reviewBlockList.add(new ReviewBlock("place3", (float) 4.0, "2021/01/02 05:13", ""));
+        reviewBlockList.add(new ReviewBlock("place4", (float) 3.2, "2021/01/01 07:11", ""));
+        reviewBlockList.add(new ReviewBlock("place5", (float) 3.1, "2021/01/05 10:18", ""));
+        reviewBlockList.add(new ReviewBlock("place6", (float) 3.9, "2021/01/04 22:19", "addit.comment6"));
+        reviewBlockList.add(new ReviewBlock("place7", (float) 4.5, "2021/01/08 16:13", "addit.comment7"));
+        reviewBlockList.add(new ReviewBlock("place8", (float) 5.0, "2021/01/09 19:19", ""));
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        adapter = new RecyclerAdapter(reviewBlockList);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 }
