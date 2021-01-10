ebbef7ce2fb2063d5ed6d04ef86c8af6156149b2
Michael Dressner <mdressne@terpmail.umd.edu>
2021-01-09 23:50:42 -0500

    Repositions based on search result

---
+++ app/src/main/java/com/team9/safehubs/ReviewActivity.java
@@ -0,0 +1,20 @@
+package com.team9.safehubs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.team9.safehubs.R;
+
+public class ReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+    }
+}
