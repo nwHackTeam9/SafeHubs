eca0523f11f2b37afb7d952139cb06675cae132e
Michael Dressner <mdressne@terpmail.umd.edu>
2021-01-09 20:29:27 -0500

    Repositions based on search result

--- app/src/main/java/com/team9/safehubs/MainActivity.java
+++ app/src/main/java/com/team9/safehubs/MainActivity.java
@@ -2,9 +2,16 @@ package com.team9.safehubs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,21 +22,31 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.annotations.NotNull;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity
         implements OnMapReadyCallback {
 
+    private GoogleMap map;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        Context context = this.getApplicationContext();
+        String key = context.getString(R.string.api_key);
 
         /**
          * Initialize Places. For simplicity, the API key is hard-coded. In a production
@@ -37,7 +54,7 @@ public class MainActivity extends AppCompatActivity
          */
         if (!Places.isInitialized()) {
             Places.initialize(getApplicationContext(),
-                    "AIzaSyC9AZ8jRAzrf4cX5zDI9vpTKizm-SO0zno");
+                    context.getString(R.string.api_key));
         }
 
         // Initialize the AutocompleteSupportFragment.
@@ -49,8 +66,42 @@ public class MainActivity extends AppCompatActivity
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
-                // TODO: Get info about the selected place.
-                Log.i("TAG", "Place: " + place.getName() + ", " + place.getId());
+
+                String url = "https://maps.googleapis.com/maps/api/place/details/json?" +
+                        "key=" + key + "&place_id=" + place.getId();
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    JSONObject location = response.getJSONObject("result")
+                                            .getJSONObject("geometry").getJSONObject("location");
+
+                                    Double lat = location.getDouble("lat");
+                                    Double lng = location.getDouble("lng");
+                                    LatLng latLng = new LatLng(lat, lng);
+
+                                    if (latLng != null) {
+                                        map.addMarker(new MarkerOptions()
+                                                .position(latLng)
+                                                .title(place.getName()));
+                                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,
+                                                10f));
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // TODO: Handle error
+
+                            }
+                        }
+                      );
+                queue.add(jsonObjectRequest);
             }
 
             @Override
@@ -69,9 +120,11 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
         LatLng sydney = new LatLng(-33.852, 151.211);
         googleMap.addMarker(new MarkerOptions()
                 .position(sydney)
                 .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10f));
     }
 }
