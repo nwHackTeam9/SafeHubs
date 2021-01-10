855ce59236d985299bb2fa87b47a4118c487c443
Michael Dressner <mdressne@terpmail.umd.edu>
2021-01-10 12:10:36 -0500

    Created ReadReviewsActivity

---
+++ app/src/main/java/com/team9/safehubs/ReadReviewsActivity.java
@@ -0,0 +1,14 @@
+package com.team9.safehubs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReadReviewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_reviews);
+    }
+}
